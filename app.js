7b6f56274bbf01c3bf6fa760420097f0a8b0f572
Andy Finnell <andy.finnell@gmail.com>
2017-06-24 05:48:43 +0000

    added new/create routes and form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose"),
+var bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
     express =  require("express"),
     app =      express();
     
@@ -7,22 +8,14 @@ mongoose.connect("mongodb://localhost/chronicles");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-var playerSchema = new mongoose.Schema({
-    name: String,
-    page: String
-});
-
-var Player = mongoose.model("Player", playerSchema);
 
 var sessionSchema = new mongoose.Schema({
     title: String,
     content: String,
     image: String,
-    party: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Player"
-    }],
+    party: [String],
     date: String
 });
 
@@ -32,7 +25,26 @@ app.get("/", function(req, res){
    res.render("index"); 
 });
 
+app.get("/new", function(req, res){
+   res.render("new"); 
+});
 
+app.post("/", function(req, res){
+   var title = req.body.title,
+       image = req.body.image,
+       date = req.body.date,
+       party = req.body.party,
+       content = req.body.content,
+       newSession = {title: title, image: image, date: date, party: party, content: content}
+       Session.create(newSession, function(err, createdSession){
+           if (err){
+               console.log(err);
+           } else {
+               console.log(createdSession);
+               res.redirect("/");
+           }
+       });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
