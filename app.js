925bb232612fc99814b934573d2f216ea019b07e
Andy Finnell <andy.finnell@gmail.com>
2017-06-24 00:22:12 +0000

    db schema complete

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ mongoose.connect("mongodb://localhost/chronicles");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+var playerSchema = new mongoose.Schema({
+    name: String,
+    page: String
+});
+
+var Player = mongoose.model("Player", playerSchema);
 
 var sessionSchema = new mongoose.Schema({
     title: String,
     content: String,
     image: String,
-    party: [String],
+    party: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Player"
+    }],
     date: String
-})
+});
 
 var Session = mongoose.model("Session", sessionSchema);
 
