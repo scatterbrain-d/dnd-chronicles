06e9d8f85582bd5801412fb929a8ec9ee02c85e8
Andy Finnell <andy.finnell@gmail.com>
2017-06-24 00:14:31 +0000

    basic layout & initial db hookup

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var express = require("express"),
-    app =     express();
+var mongoose = require("mongoose"),
+    express =  require("express"),
+    app =      express();
     
 
+mongoose.connect("mongodb://localhost/chronicles");
+
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
+var sessionSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    image: String,
+    party: [String],
+    date: String
+})
 
+var Session = mongoose.model("Session", sessionSchema);
 
+app.get("/", function(req, res){
+   res.render("index"); 
+});
 
 
 
