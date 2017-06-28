94fc42a400745ba9fe0fe8fd6de24fb73706089d
Andy Finnell <andy.finnell@gmail.com>
2017-06-28 03:14:13 +0000

    added character schema, updated session

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
-var bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    express =  require("express"),
-    app =      express();
+var methodOverride = require("method-override"),
+    bodyParser =     require("body-parser"),
+    mongoose =       require("mongoose"),
+    express =        require("express"),
+    app =            express();
     
+//==========================================
 
-mongoose.connect("mongodb://localhost/chronicles");
+mongoose.connect("mongodb://localhost/chronicles_2");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+// TO DO
+// Add DM,img caption to session schema
+// party name shortcut doesn't work with spaces or some other characters - make schema or convert somehow
+//==========================================
+
+
+var characterSchema = new mongoose.Schema({
+    name: String,
+    level: Number,
+    gameClass: String,
+    skill: String,
+    image: String,
+    likes: String,
+    dislikes: String
+});
+
+var Character = mongoose.model("Character", characterSchema);
 
 
 var sessionSchema = new mongoose.Schema({
     title: String,
     content: String,
     image: String,
-    party: [String],
+    caption: String,
+    dm: String,
+    characters: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Character"
+    }],
     date: String
 });
 
 var Session = mongoose.model("Session", sessionSchema);
+
+//==========================================
+
 
 app.get("/", function(req, res){
     Session.find({}, function(err, sessions){
@@ -41,7 +70,7 @@ app.post("/", function(req, res){
        date = req.body.date,
        party = req.body.party,
        content = req.body.content,
-       newSession = {title: title, image: image, date: date, party: party, content: content}
+       newSession = {title: title, image: image, date: date, party: party, content: content};
        Session.create(newSession, function(err, createdSession){
            if (err){
                console.log(err);
@@ -50,6 +79,41 @@ app.post("/", function(req, res){
                res.redirect("/");
            }
        });
+});
+
+app.get("/:id", function(req, res){
+    Session.findById(req.params.id, function(err, foundSession){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("show", {session: foundSession});
+        }
+    })
+})
+
+
+
+app.get("/:id/edit", function(req, res){
+   Session.findById(req.params.id, function(err, foundSession){
+       if (err){
+           console.log(err);
+           res.redirect("/");
+       } else {
+           res.render("edit", {session: foundSession});
+       }
+   }); 
+});
+
+app.put("/:id", function(req, res){
+    req.body.session.party = req.body.newParty;
+    Session.findByIdAndUpdate(req.params.id, req.body.session, function(err, updatedSession){
+        if (err){
+            console.log(err);
+            res.redirect("/");
+        } else {
+            res.redirect("/" + req.params.id);
+        }
+    });
 });
 
 
